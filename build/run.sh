6eebb9c158f8c5d0aafe6de25894c329c1329a1d
Eduardo Antuña Díez <eduadiez@gmail.com>
2020-09-24 12:55:54 +0200

    first commit

---
+++ build/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+PASSWORD=${PASSWORD:-}
+DATADIR=${DATADIR:-/home/bee/.bee}
+EXTRA_OPTS=${EXTRA_OPTS:-}
+
+VERSION=`bee version`
+echo "Running bee:"
+echo $VERSION
+
+echo $PASSWORD > /home/bee/.password
+
+exec bee start --config /home/bee/config.yml $EXTRA_OPTS $@ 2>&1
