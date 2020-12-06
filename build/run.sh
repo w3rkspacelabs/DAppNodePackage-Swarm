a1c34c3ed1abe80beca68bf095d237545bca623f
Eduardo Antuña Díez <eduadiez@gmail.com>
2020-12-06 16:08:45 +0100

    Improved setup wizard and fix bakcups

--- build/run.sh
+++ build/run.sh
@@ -12,6 +12,14 @@ VERSION=`bee version`
 echo "Running bee:"
 echo $VERSION
 
+# db capacity in chunks, multiply by 4096 to get approximate capacity in bytes
+BEE_DB_CAPACITY=${BEE_DB_CAPACITY:-5000000}
+
+# send a welcome message string during handshakes
+BEE_WELCOME_MESSAGE=${BEE_WELCOME_MESSAGE:-"Hello from my DAppNode!"}
+
 echo $PASSWORD > /home/bee/.password
 
-exec bee start --swap-endpoint https://rpc.slock.it/goerli --config /home/bee/config.yml $EXTRA_OPTS $@ 2>&1
+chown -R bee:bee /home/bee/.bee
+
+exec gosu bee:bee bee start --config /home/bee/config.yml $EXTRA_OPTS $@ 2>&1
