648a3f8c270a173710c1550c61dcf511eb748a10
mfw78 <mfw78@protonmail.com>
2022-09-14 13:07:44 +0000

    feat: password configuration

--- bee/entrypoint.sh
+++ bee/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# from `ethersphere/bee` create a password for use
+if ! test -f /home/bee/.bee/password; then
+    < /dev/urandom tr -dc _A-Z-a-z-0-9 2> /dev/null | head -c32 > /home/bee/.bee/password
+    chmod 0600 /home/bee/.bee/password
+    chown bee:bee /home/bee/.bee/password
+fi
 
 exec bee start ${EXTRA_OPTS}
